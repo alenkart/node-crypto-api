39332ec0c41929493e0973376df5e6a5cf36ece3
Alenkart Rodriguez (ClaroDom) <alenkart.r@gmail.com>
2018-01-04 14:04:46 -0400

    Added support to multiple apis to the Market class

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-const { Cexio, CoinMarketCap, Bittrex, Cryptonator, Kraken } = require('./lib');
+/*
+	Alenkart Rodriguez
+*/
 
-kraken = new Kraken();
-bittrex = new Bittrex();
-cexio = new Cexio();
-coinMarketCap = new CoinMarketCap();
-cryptonator = new Cryptonator();
+'use strict';
 
-kraken.ticker()
-	.then(console.log)
-	.then(() => cexio.ticker())
-	.then(res => console.log('\n\n', res))
-	.then(() => cryptonator.ticker())
-	.then(res => console.log('\n\n', res))
-	.then(() => coinMarketCap.ticker())
-	.then(res => console.log('\n\n', res))
-	.then(() => bittrex.getticker())
-	.then(res => console.log('\n\n', res))
-	.then(() => {
-
-		cexio
-		.socket()
-		.send({ "e": "subscribe", "rooms": [ "tickers" ] })
-		.init();
-
-	}).catch(console.error);
+module.exports = require('./lib');
